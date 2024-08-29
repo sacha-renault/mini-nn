f49a1aaac63fcf6f7697848514f8f3ca265e3a04
sacha-renault <sacha35340@gmail.com>
2024-08-29 10:31:40 +0200

    fix: activation put all activation in a single file

--- src/mini-nn/src/ActivationFunction.hpp
+++ src/mini-nn/src/ActivationFunction.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #include <cmath>
+#include <algorithm>
+#include <numeric>
 #include "Value.hpp"
 
 namespace Activations {
-    class ActivationFunction{
-    protected:
-        virtual float _forward(float x) = 0;
-        virtual float _backward(float x) = 0;
+    class BaseActivation { 
     public:
+        virtual std::vector<Value> operator()(std::vector<Value>& inputs) = 0;
+    };
+
+
+    class LambdaActivation : public BaseActivation {
+    protected:
+        std::function<float(float)> _forward_func;
+        std::function<float(float)> _backward_func;
+
+        float _forward(float x) {
+            return _forward_func(x);
+        }
+        float _backward(float x) {
+            return _backward_func(x);
+        } 
+
+    public:
+        LambdaActivation(std::function<float(float)> forward, std::function<float(float)> backward)
+            : _forward_func(forward), _backward_func(backward) { }
+
         virtual Value operator()(Value& other) final {
             // TODO push_back in backward 
             float x = other.getData();
@@ -25,23 +44,63 @@ namespace Activations {
 
             return out;
         }
+
+        virtual std::vector<Value> operator()(std::vector<Value>& inputs) final {
+            std::vector<Value> outputs; 
+
+            for (auto &val : inputs) {
+                outputs.push_back((*this)(val));
+            }
+
+            return outputs;
+        }
     };
 
-    class LambdaActivation : public ActivationFunction {
+    class ActivationFunctionVectorLambda : public BaseActivation{
     protected:
-        std::function<float(float)> _forward_func;
-        std::function<float(float)> _backward_func;
+        std::function<std::vector<float>(const std::vector<float>& x)> _forward_func;
+        std::function<std::vector<float>(const std::vector<float>& x)> _backward_func;
 
-        float _forward(float x) {
+        virtual std::vector<float> _forward(const std::vector<float>& x){
             return _forward_func(x);
-        }
-        float _backward(float x) {
+        };
+        virtual std::vector<float> _backward(const std::vector<float>& x){
             return _backward_func(x);
-        } 
+        };
 
     public:
-        LambdaActivation(std::function<float(float)> forward, std::function<float(float)> backward)
+        ActivationFunctionVectorLambda(std::function<std::vector<float>(const std::vector<float>& x)> forward, std::function<std::vector<float>(const std::vector<float>& x)> backward)
             : _forward_func(forward), _backward_func(backward) { }
+
+        std::vector<Value> operator()(std::vector<Value>& inputs) {
+            std::vector<float> x(inputs.size());
+            for (size_t i = 0; i < inputs.size(); ++i) {
+                x[i] = inputs[i].getData();
+            }
+
+            std::vector<float> softmax_out = _forward(x);
+
+            std::vector<Value*> children(inputs.size());
+            for (size_t i = 0; i < inputs.size(); ++i) {
+                children[i] = &inputs[i];
+            }
+
+            std::vector<Value> outputs;
+            for (size_t i = 0; i < softmax_out.size(); ++i) {
+                outputs.emplace_back(Value(softmax_out[i], children));
+            }
+
+            for (size_t i = 0; i < outputs.size(); ++i) {
+                outputs[i].setBackward([&, i]() {
+                    std::vector<float> grad_softmax = _backward(softmax_out);
+                    for (size_t j = 0; j < inputs.size(); ++j) {
+                        inputs[j].setGrad(grad_softmax[j] * outputs[i].getGrad());
+                    }
+                });
+            }
+
+            return outputs;
+        }
     };
 
     // ReLU activation function
@@ -53,12 +112,45 @@ namespace Activations {
     // Tanh activation function
     static LambdaActivation Tanh(
         [](float x) { return std::tanh(x); },                // Forward: Tanh
-        [](float x) { float tanhx = std::tanh(x); return 1.0f - tanhx * tanhx; }  // Backward: Tanh derivative
+        [](float x) { return 1.0f - x * x; }  // Backward: Tanh derivative
     );
 
     // Sigmoid activation function
     static LambdaActivation Sigmoid(
         [](float x) { return 1.0f / (1.0f + std::exp(-x)); },  // Forward: Sigmoid
-        [](float x) { float sigx = 1.0f / (1.0f + std::exp(-x)); return sigx * (1.0f - sigx); }  // Backward: Sigmoid derivative
+        [](float x) { return x * (1.0f - x); }  // Backward: Sigmoid derivative
+    );
+
+    // Softmax activation function
+    static ActivationFunctionVectorLambda Softmax(
+        // Forward function
+        [](const std::vector<float>& x) -> std::vector<float> {
+            std::vector<float> exp_x(x.size());
+            float max_val = *std::max_element(x.begin(), x.end());
+            
+            // Compute exponentials for numerical stability
+            for (size_t i = 0; i < x.size(); ++i) {
+                exp_x[i] = std::exp(x[i] - max_val);
+            }
+            
+            float sum_exp_x = std::accumulate(exp_x.begin(), exp_x.end(), 0.0f);
+
+            // Normalize to get probabilities
+            for (size_t i = 0; i < exp_x.size(); ++i) {
+                exp_x[i] /= sum_exp_x;
+            }
+
+            return exp_x;
+        },
+        
+        // Backward function
+        [](const std::vector<float>& softmax_output) -> std::vector<float> {
+            std::vector<float> grad(softmax_output.size());
+            // Compute the gradient for each output using the simplified diagonal Jacobian form
+            for (size_t i = 0; i < softmax_output.size(); ++i) {
+                grad[i] = softmax_output[i] * (1.0f - softmax_output[i]);
+            }
+            return grad;
+        }
     );
 }
