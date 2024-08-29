41a9daa5c53355e8a3c25be0805aab39bc1bbfce
sacha-renault <sacha35340@gmail.com>
2024-08-29 08:08:35 +0200

    chore: ActivationFunction change location of instanciation

--- src/mini-nn/src/ActivationFunction.hpp
+++ src/mini-nn/src/ActivationFunction.hpp
@@ -43,4 +43,22 @@ namespace Activations {
         LambdaActivation(std::function<float(float)> forward, std::function<float(float)> backward)
             : _forward_func(forward), _backward_func(backward) { }
     };
+
+    // ReLU activation function
+    static LambdaActivation ReLU(
+        [](float x) { return std::max(0.0f, x); },           // Forward: ReLU
+        [](float x) { return x > 0.0f ? 1.0f : 0.0f; }       // Backward: ReLU derivative
+    );
+
+    // Tanh activation function
+    static LambdaActivation Tanh(
+        [](float x) { return std::tanh(x); },                // Forward: Tanh
+        [](float x) { float tanhx = std::tanh(x); return 1.0f - tanhx * tanhx; }  // Backward: Tanh derivative
+    );
+
+    // Sigmoid activation function
+    static LambdaActivation Sigmoid(
+        [](float x) { return 1.0f / (1.0f + std::exp(-x)); },  // Forward: Sigmoid
+        [](float x) { float sigx = 1.0f / (1.0f + std::exp(-x)); return sigx * (1.0f - sigx); }  // Backward: Sigmoid derivative
+    );
 }
