8afcda6d861eda4f4eca044be8b6cb2f60491e32
sacha-renault <sacha35340@gmail.com>
2024-08-29 20:59:13 +0200

    feat: Dense add a dense layer (fcc)

--- src/mini-nn/src/Layers/Layer.hpp
+++ src/mini-nn/src/Layers/Layer.hpp
@@ -9,13 +9,13 @@ public:
     virtual ~Layer() = default;
 
     // Pure virtual method for the forward pass
-    virtual std::vector<Value> forward(const std::vector<Value>& inputs) = 0;
+     virtual std::vector<std::shared_ptr<Value>>& forward(std::vector<std::shared_ptr<Value>>& inputs) = 0;
 
     // Pure virtual method for the backward pass (if needed)
     virtual void backward() = 0;
 
     // Pure virtual method to get the parameters of the layer
-    virtual std::vector<Parameter*> getParameters() = 0;
+    virtual std::vector<std::shared_ptr<Value>> getParameters() = 0;
 
     // Any additional virtual methods can be added here as needed
 };
