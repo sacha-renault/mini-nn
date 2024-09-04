11cb08a875bfca271b93385c4e579c76f77b9f8d
sacha-renault <sacha35340@gmail.com>
2024-09-04 15:45:07 +0200

    fix: SDG SDG wasn't in namespace opt

--- src/mini-nn/Optimizers/SGD.hpp
+++ src/mini-nn/Optimizers/SGD.hpp
@@ -1,32 +1,37 @@
 #pragma once
 #include "Optimizer.hpp"
 
-class SGD : public Optimizer
+namespace Optimizers
 {
-private:
-    std::vector<ValRef> params;
+    class SGD : public Optimizer
+    {
+    private:
+        std::vector<ValRef> params;
 
-public:
-    SGD(Model& model, float lr);
-    virtual void update(ValRef &loss) override;
-};
+    public:
+        SGD(Model& model, float lr);
+        virtual void update(ValRef &loss) override;
+    };
 
-SGD::SGD(Model& model, float lr)
-{
-    lr_ = lr;
-    params = model.getParameters();
-}
-
-void SGD::update(ValRef &loss)
-{
-    auto grad = Gradient::reverseTopologicalOrder(loss);
-    Gradient::backward(grad);
-    // Gradient::noiseGrad(grad, 1e4);
-    Gradient::clipGrad(grad, 1.5f);
-    for(auto& node : params) {
-        float grad = node->getGrad();
-        float data = node->getData();
-        node->setValue(data - grad * lr_);
+    SGD::SGD(Model& model, float lr)
+    {
+        lr_ = lr;
+        params = model.getParameters();
     }
-    Gradient::zeroGrad(grad);
-}
+
+    void SGD::update(ValRef &loss)
+    {
+        auto grad = Gradient::reverseTopologicalOrder(loss);
+        Gradient::backward(grad);
+        // Gradient::noiseGrad(grad, 1e4);
+        Gradient::clipGrad(grad, 1.5f);
+        for(auto& node : params) {
+            float grad = node->getGrad();
+            float data = node->getData();
+            node->setValue(data - grad * lr_);
+        }
+        Gradient::zeroGrad(grad);
+    }
+} // namespace Optimizers
+
+
